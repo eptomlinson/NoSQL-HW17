80ad3c1e5198493e27bae2432ceebe8504536004
Erik Tomlinson <eptomlinson@gmail.com>
2020-11-12 09:14:38 -0600

    cleaned up code, deleted unused commented code

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -40,26 +40,4 @@ module.exports = app => {
                 res.json(dbWorkout)
             })
     })
-}
-//     router.post("/api/transaction/bulk", ({ body }, res) => {
-//         Transaction.insertMany(body)
-//     .then(dbTransaction => {
-//       res.json(dbTransaction);
-//     })
-//     .catch(err => {
-//       res.status(400).json(err);
-//     });
-// });
-
-// router.get("/api/transaction", (req, res) => {
-//   Transaction.find({})
-//     .sort({ date: -1 })
-//     .then(dbTransaction => {
-//       res.json(dbTransaction);
-//     })
-//     .catch(err => {
-//       res.status(400).json(err);
-//     });
-// });
-
-// module.exports = router;
+};
